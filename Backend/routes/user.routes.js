89adff0595159d75126a86eebfebc47186b5a35c
guigui-payfit <guillaume@payfit.com>
2023-09-29 16:12:06 +0200

    style(architect-portfolio): prettify code using prettier (except fo HTML)

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/users.controller');
+const express = require('express')
+const router = express.Router()
+const userCtrl = require('../controllers/users.controller')
 
-router.post('/login', userCtrl.login);
+router.post('/login', userCtrl.login)
 //router.post('/signup', userCtrl.signup);
 
-module.exports = router;
+module.exports = router
